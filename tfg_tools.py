fa668360a995372c695acf9d61bc84a86c4bad74
obarquero <oscar.barquero@urjc.es>
2018-02-02 16:00:30 +0100

    Added database, change in utils to perform the previous analysis

--- tfg_tools.py
+++ tfg_tools.py
@@ -10,23 +10,26 @@ import sys
 import wfdb
 import numpy as np
 import scipy.signal as sg
+import os
+import glob
+import pandas as pd
+import matplotlib.pyplot as plt
 
-
-def read_header(fname):
+def read_header(fname,path):
     """
     Funcion para leer el header de las señales
     """
     
     idx_point = fname.find('.')
     fname = fname[:idx_point]
-    header = wfdb.rdheader(fname)
+    header = wfdb.rdheader(path+fname)
     
     return header
  
 
 #Función para leer las señales
 
-def read_challenge_mat_files(fname):
+def read_challenge_mat_files(fname,path):
     """
     Lee las señales .mat del challenge 2017 y devuelve un array de numpy
 
@@ -47,9 +50,9 @@ def read_challenge_mat_files(fname):
     #header = read_header(fname_header)
 
     #read mat file
-    header = read_header(fname)
+    header = read_header(fname,path)
 
-    mat_file = io.loadmat(fname)
+    mat_file = io.loadmat(path+fname)
 
     #mat_file is a dictionary, and the data is on the 'var' key
 
@@ -77,6 +80,90 @@ def processing_ecg(ecg,fs = 300):
     
     return ecg_filtered
 
+def get_class(header):
+    """
+    Function that given a header from a record return the class it belongs
+    """
+    
+    
+    
+    classes = np.loadtxt('./physionet_challenge/training2017/REFERENCE.csv',delimiter = ',',dtype = 'str')
+    
+    ids = list(classes[:,0])
+    classes = list(classes[:,1])
+    
+    id_h = header['recordname'] #get recordname
+    
+    #TO DO? Control if there exist id_h in ids
+    idx = ids.index(id_h) #get idx to obtain class
+
+    #return class for the current record
+    return classes[idx]
+    
+
+def plot_all_records(plot_original = True):
+    """
+    Function that plot all records
+    """
+    
+    curr_dir = os.getcwd()
+    #os.chdir('./physionet_challenge/training2017/')
+    path = './physionet_challenge/training2017/'
+    
+    #TO DO
+    #implement some mechanism to abort execution
+    #for over .mat
+    for fname in list(glob.glob(path+'*.mat')):
+        
+        #read ecg
+        ecg,header = read_challenge_mat_files(os.path.basename(fname),path)
+        
+        #function to get the the class
+        class_ecg = get_class(header)
+        thesaurus = {'N':'Normal','A':'AF','O':'Other Rhyth','~':'Noisy'}
+        
+        #plot current ecg
+        t = np.arange(len(ecg))/float(header['fs'])
+        
+        #preprocessing ecg
+        ecg_filtered = processing_ecg(ecg,fs = 300)
+        tit = 'ECG. Record: '+ header['recordname'] + ' Class: '+ thesaurus[class_ecg]
+        
+        plt.close('all')
+        
+        if plot_original:
+            plt.plot(t,ecg,label = 'ecg original')
+            
+        plt.plot(t,ecg_filtered,label = 'ecg_filtered')
+        plt.xlabel('Time (sec)')
+        plt.title(tit)
+        plt.ylabel('Amplitude (mV)')
+        plt.axis('tight')
+
+        plt.waitforbuttonpress()
+            
+        
+def get_distribution_classes():
+    """
+    Function to get the distribution of differente clases
+    """
+    
+    #MA tu código aquí
+    
+    #leer references.csv y recontar el número de cada clase
+    
+def get_distribution_length():
+    """
+    Function to get a distribution of the signal lengths
+    """
+    
+    #MA tu código aquí
+    
+    #hacer un for sobre todos los datos, obtener la longitud de cada señal y guardarla en una lista
+    
+    
+    
+
 #to modify
 """
 def deep_conv_lstm_net():
@@ -103,3 +190,5 @@ def deep_conv_lstm_net():
 #if __name__ == "__main__":
 
 #    read_challenge_mat_files(sys.argv[1])
+
+plot_all_records(plot_original=False)
